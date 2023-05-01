49c785c11eeb3e201670d0a89a3ac91e056ecf28
レミィログ <sdu@outlook.jp>
2023-05-01 21:42:10 +0900

    プログラム更新
    スイッチ切り替えで点滅するモードを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,64 @@
 #include <Arduino.h>
 
-// LEDのピン番号を定義
-int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+// LEDを制御するためのピン番号
+const byte LED_PINS[] = {2, 3, 4, 5, 6, 7, 8, 9};
 
-// 可変抵抗器のピン番号を定義
-int potPin = A0;
+// LEDのピン番号を定義
+int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+// ピン11の状態を保存する変数
+int pin11State = LOW;
+
+// LED点滅のための変数
+unsigned long previousMillis = 0; // 前回LEDが点滅した時間
+const long interval = 500;        // LEDの点滅周期
 
 void setup()
 {
   // 各LEDのピンをOUTPUTに設定
   for (int i = 0; i < 9; i++)
   {
+    pinMode(LED_PINS[i], OUTPUT);
     pinMode(ledPins[i], OUTPUT);
   }
+
+  pinMode(11, INPUT); // ピン11を入力モードに設定する
+  Serial.begin(9600); // シリアル通信の初期化
 }
 
 void loop()
 {
-  // 可変抵抗器からの入力値を取得
-  int speed = analogRead(potPin);
-  // LEDを交互に点滅させる
-  for (int i = 0; i < 9; i++)
+  int buttonState = digitalRead(11); // ピン11の状態を読み取る
+  Serial.println(buttonState);       // 状態をシリアルモニタに出力する
+
+  // ピン11がHighになった場合
+  if (buttonState == HIGH)
   {
-    digitalWrite(ledPins[i], HIGH); // LEDを点灯
-    delay(speed);                   // 可変抵抗器の値に応じて待機時間を設定
-    digitalWrite(ledPins[i], LOW);  // LEDを消灯
+    for (int i = 0; i < 9; i++)
+    {
+      digitalWrite(LED_PINS[i], HIGH); // LEDを点灯させる
+    }
+    delay(500); // 0.5秒待つ
+    for (int i = 0; i < 9; i++)
+    {
+      digitalWrite(LED_PINS[i], LOW); // LEDを消灯させる
+    }
+    delay(500);
+  }
+  else
+  {                                         // Lowになった場合
+    unsigned long currentMillis = millis(); // 現在の時間を取得する
+    if (currentMillis - previousMillis >= interval)
+    {                                 // interval時間経過した場合
+      previousMillis = currentMillis; // 前回LEDが点滅した時間を更新する
+      // LEDを交互に点滅させるプログラムを実行する
+      int speed = analogRead(A0);
+      for (int i = 0; i < 9; i++)
+      {
+        digitalWrite(ledPins[i], HIGH);
+        delay(speed);
+        digitalWrite(ledPins[i], LOW);
+      }
+    }
   }
 }
